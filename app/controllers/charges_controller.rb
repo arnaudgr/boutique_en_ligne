e1c16a6bac3f6b2302a269baf065aac6c153014c
Airvanne <erwanb@lilo.org>
2018-11-27 19:41:24 +0100

    stripe

---
+++ app/controllers/charges_controller.rb
@@ -0,0 +1,17 @@
+class ChargesController < ApplicationController
+  def new
+  end
+
+  def create
+    # Amount in cents
+    @amount = 2500
+
+    customer = Stripe::Customer.create(email: 'soul.kitchen.rennes@gmail.com', card: params[:stripeToken])
+
+    charge = Stripe::Charge.create(customer: customer.id, amount: @amount, description: 'SoulKitchen', currency: 'eur')
+
+  rescue Stripe::CardError => e
+    flash[:error] = e.message
+    redirect_to charges_path
+  end
+end
