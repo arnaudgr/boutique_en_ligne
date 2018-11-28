184e787157ad1fe4b992bfaaf1b51f139fce4dea
Airvanne <erwanb@lilo.org>
2018-11-28 19:46:26 +0100

    connexion stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,14 +1,21 @@
 class ChargesController < ApplicationController
   def new
+    puts 'CONTROLLLER CHARGES#NEWwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww'
+    @amount = params[:amount]
   end
 
   def create
     # Amount in cents
-    @amount = 1000
+    puts 'COONTROLLLLLERRRRRRRRRR CHARGES#CREATE'
+    puts params
+    @amount = params[:format]
 
     customer = Stripe::Customer.create(email: 'soul.kitchen.rennes@gmail.com', card: params[:stripeToken])
 
     charge = Stripe::Charge.create(customer: customer.id, amount: @amount, description: 'SoulKitchen', currency: 'eur')
+
+    redirect_to root_path
+    flash[:notice] = "Votre commande de #{params[:format]}centimes a bien été effectuée"
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
